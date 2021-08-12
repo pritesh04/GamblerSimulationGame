1c41c33524fdf55938c3c70e0bf1e1400061bd3c
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-12 12:56:58 +0530

    [Pritesh ] - Amount remained after losing and winning

--- src/com/Bridgelab/Workshop/GamblingSimulation.java
+++ src/com/Bridgelab/Workshop/GamblingSimulation.java
@@ -3,20 +3,19 @@ package com.Bridgelab.Workshop;
 public class GamblingSimulation {
 	public static int amountPerDay = 100;
 	public static int betPerDay = 1;
-	
 
 	public void WinnerLoser(int result) {
 
 		if (result == 1) {
-			amountPerDay += 50 ;
+			amountPerDay += 50;
 			System.out.println("Winner");
-			System.out.println("Amount remained is " + amountPerDay);
 
 		} else {
 			amountPerDay -= 50;
 			System.out.println("Losser");
-			System.out.println("Amount remained is " + amountPerDay);
+
 		}
+		System.out.println("Amount remained is " + amountPerDay);
 	}
 
 	public static void main(String[] args) {
