96ccbe3e1ba288ab7a70a4a98f1077767ffac0c6
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-12 12:22:39 +0530

    Decided wether Winner or Losser

--- src/com/Bridgelab/Workshop/GamblingSimulation.java
+++ src/com/Bridgelab/Workshop/GamblingSimulation.java
@@ -1,8 +1,27 @@
 package com.Bridgelab.Workshop;
 
 public class GamblingSimulation {
-public static void main(String[] args) {
-	int amountPerDay=100;
-	int betPerDay=1;
-}
+	public static int amountPerDay = 100;
+	public static int betPerDay = 1;
+
+	public static void WinnerLoser(int result)
+	{
+		
+		if (result == 1) {
+			amountPerDay += 1;
+			System.out.println("Winner");
+			
+		} else {
+			amountPerDay -= 1;
+			System.out.println("Losser");
+		}
+	}
+	public static void main(String[] args) {
+		int result = (int) Math.floor(Math.random() * 10) % 2;
+		System.out.println(result);
+
+		WinnerLoser(result);
+
+		
+	}
 }
