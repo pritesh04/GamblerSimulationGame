eb6f7311013c4e4aab1d24e616ce3782137cb310
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-12 12:48:26 +0530

    Final Amount after winning and losing

--- src/com/Bridgelab/Workshop/GamblingSimulation.java
+++ src/com/Bridgelab/Workshop/GamblingSimulation.java
@@ -3,25 +3,26 @@ package com.Bridgelab.Workshop;
 public class GamblingSimulation {
 	public static int amountPerDay = 100;
 	public static int betPerDay = 1;
+	
 
-	public static void WinnerLoser(int result)
-	{
-		
+	public void WinnerLoser(int result) {
+
 		if (result == 1) {
-			amountPerDay += 1;
+			amountPerDay += 50 ;
 			System.out.println("Winner");
-			
+			System.out.println("Amount remained is " + amountPerDay);
+
 		} else {
-			amountPerDay -= 1;
+			amountPerDay -= 50;
 			System.out.println("Losser");
+			System.out.println("Amount remained is " + amountPerDay);
 		}
 	}
+
 	public static void main(String[] args) {
+		GamblingSimulation gamblingSimulation = new GamblingSimulation();
 		int result = (int) Math.floor(Math.random() * 10) % 2;
-		System.out.println(result);
+		gamblingSimulation.WinnerLoser(result);
 
-		WinnerLoser(result);
-
-		
 	}
 }
