e530cc34e57ef80f7c07210df02536770c8ed9cf
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-13 10:15:59 +0530

    Decided Amount after Winning and Losing

--- src/com/Bridgelab/Workshop/GamblingSimulation.java
+++ src/com/Bridgelab/Workshop/GamblingSimulation.java
@@ -4,24 +4,45 @@ public class GamblingSimulation {
 	public static int amountPerDay = 100;
 	public static int betPerDay = 1;
 
-	public void WinnerLoser(int result) {
+	public int WinnerLoser(int result) {
 
+		result = (int) Math.floor(Math.random() * 10) % 2;
 		if (result == 1) {
-			amountPerDay += 50;
+
 			System.out.println("Winner");
 
 		} else {
-			amountPerDay -= 50;
+
 			System.out.println("Losser");
 
 		}
-		System.out.println("Amount remained is " + amountPerDay);
+		System.out.println("Amount remained is "+amountPerDay);return amountPerDay;
 	}
+
+	
 
 	public static void main(String[] args) {
+		int won = 0, los = 0;
+		int[] a = new int[30];
+		int stake = amountPerDay;
 		GamblingSimulation gamblingSimulation = new GamblingSimulation();
 		int result = (int) Math.floor(Math.random() * 10) % 2;
-		gamblingSimulation.WinnerLoser(result);
+		
 
+		gamblingSimulation.WinnerLoser(result);
+		System.out.println("tota amount after 20 days is " + amountPerDay);
+		while (amountPerDay > (stake * 0.5) && amountPerDay < (stake * 1.5)) {
+			double random = Math.random();
+			if (random > 0.5) {
+				amountPerDay += betPerDay;
+			} else {
+				amountPerDay -= betPerDay;
+			}
+		}
+		if (amountPerDay > (stake * 0.5)) {
+			System.out.println("Gambler loss 50%");
+		} else
+			System.out.println("Gambler gain 50%  ");
 	}
+
 }
