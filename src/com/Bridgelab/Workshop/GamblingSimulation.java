23cfde1a32a67a767468a76421847ea4deb68acd
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-14 10:30:51 +0530

    Calculated Winning amount and losing amount

--- src/com/Bridgelab/Workshop/GamblingSimulation.java
+++ src/com/Bridgelab/Workshop/GamblingSimulation.java
@@ -3,46 +3,47 @@ package com.Bridgelab.Workshop;
 public class GamblingSimulation {
 	public static int amountPerDay = 100;
 	public static int betPerDay = 1;
+	public static int stake = amountPerDay;
+	public static double gain = amountPerDay * 0.5;
+	public static double loss = amountPerDay * 0.5;
 
-	public int WinnerLoser(int result) {
-
+	public static String WinnerLoser() {
+		int result;
 		result = (int) Math.floor(Math.random() * 10) % 2;
 		if (result == 1) {
 
-			System.out.println("Winner");
-
+			return "winner";
 		} else {
 
-			System.out.println("Losser");
+			return "losser";
 
 		}
-		System.out.println("Amount remained is "+amountPerDay);return amountPerDay;
+
 	}
 
-	
+	public static void ResignDay() {
 
-	public static void main(String[] args) {
-		int won = 0, los = 0;
-		int[] a = new int[30];
-		int stake = amountPerDay;
-		GamblingSimulation gamblingSimulation = new GamblingSimulation();
-		int result = (int) Math.floor(Math.random() * 10) % 2;
-		
+		while (amountPerDay > (loss) && stake < (gain)) {
 
-		gamblingSimulation.WinnerLoser(result);
-		System.out.println("tota amount after 20 days is " + amountPerDay);
-		while (amountPerDay > (stake * 0.5) && amountPerDay < (stake * 1.5)) {
-			double random = Math.random();
-			if (random > 0.5) {
-				amountPerDay += betPerDay;
+			if (WinnerLoser() == "Winning") {
+				stake += betPerDay;
 			} else {
-				amountPerDay -= betPerDay;
+				stake -= betPerDay;
 			}
 		}
-		if (amountPerDay > (stake * 0.5)) {
-			System.out.println("Gambler loss 50%");
-		} else
-			System.out.println("Gambler gain 50%");
+
 	}
 
+	public static void main(String[] args) {
+
+		GamblingSimulation gamblingSimulation = new GamblingSimulation();
+
+		gamblingSimulation.WinnerLoser();
+		System.out.println("tota amount after 20 days is " + amountPerDay);
+		ResignDay();
+		if (amountPerDay < 100)
+			System.out.println("Gambler loss and total amount = " + amountPerDay);
+		else
+			System.out.println("Gambler win and total amount =" + amountPerDay);
+	}
 }
